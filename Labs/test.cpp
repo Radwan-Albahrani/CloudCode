85f601aa83ce0c911ae33b068dfd57eed35ef6ca
JackyXteam <radwan158@gmail.com>
2021-11-07 07:25:01 +0000

    Testing Arrays. Useless

--- Labs/test.cpp
+++ Labs/test.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main(int argc, const char** argv) 
 {
-    cout << "Hello";
+    int testing[5];
+    for (size_t i = 0; i < 5; i++)
+    {
+        testing[i] = i*2+1;
+    }
+    
+    cout << testing[4];
     return 0;
 }
