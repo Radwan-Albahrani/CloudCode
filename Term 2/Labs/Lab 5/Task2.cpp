520c47bdede4b4da8c18be3b49fcbfc0fa37ff85
JackyXteam <radwan158@gmail.com>
2022-02-28 11:05:46 +0000

    Task1 and Task2

---
+++ Term 2/Labs/Lab 5/Task2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+int evaluate(int, int);
+int main(int argc, char const *argv[])
+{
+    // Randomizing number
+    srand(time(0));
+
+    // Generate number between 1 and 50
+    int randomNumber = rand() % 50;
+    
+    // Get number from user
+    int user = 0;
+    cout << "Guessing game started!" << endl;
+    do
+    {
+        cout << "Guess: ";
+        cin >> user;
+
+    } while (evaluate(user, randomNumber) != 0);
+    cout << "Random number GUESSED!" << endl;
+    return 0;
+}
+
+int evaluate(int user, int randomNumber)
+{
+    if(randomNumber > user)
+    {
+        cout << "Random number is Greater!" << endl;
+        return 1;
+    }
+    else if (randomNumber < user)
+    {
+        cout << "Random number is Lower!" << endl;
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
