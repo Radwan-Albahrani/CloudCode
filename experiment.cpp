96e2693e203a8019a1e241c6c8509f13bd8050f7
Radwan albahrani <radwan158@gmail.com>
2022-03-21 15:00:06 +0300

    Quick Edits

--- experiment.cpp
+++ experiment.cpp
@@ -6,7 +6,7 @@ using namespace std;
 struct Grades
 {
     double percentage;
-    int credit;
+    int credit = 0;
 };
 
 
@@ -27,46 +27,47 @@ int main(int argc, char const *argv[])
     // Necessary counters
     int studentCounter = 0;
     int coursePerStudent[100] = {0};
-    // Testing phase: Create student and add random info
-    Student student[100];
-    student[0].Name = "Ahmad";
-    student[0].courses[0].percentage = 85;
-    student[0].courses[0].credit = 3;
+    
+    // Testing phase: Create students and add random info
+    Student students[100];
+    students[0].Name = "Ahmad";
+    students[0].courses[0].percentage = 85;
+    students[0].courses[0].credit = 3;
     
     // Counter for how many students added and their courses
     studentCounter++;
     coursePerStudent[0]++;
 
-    // Calculate GPA of student
-    CalculateGPA(student[0], coursePerStudent[0]);
+    // Calculate GPA of students
+    CalculateGPA(students[0], coursePerStudent[0]);
 
     // Print GPA
-    cout << student[0].GPA;
+    cout << students[0].GPA;
     return 0;
 }
 
 // Function to calculate GPA by reference
-void CalculateGPA(Student& student, int size)
+void CalculateGPA(Student& students, int size)
 {
     // Prepare total points and total credit houts
     double totalpoints = 0;
     int totalhours = 0;
 
-    // Check if student has courses, and if not, break and return
-    if(student.courses[0].credit == 0)
+    // Check if students has courses, and if not, break and return
+    if(students.courses[0].credit == 0)
     {
-        cout << "Given student has no courses added.";
+        cout << "Given students has no courses added.";
         return;
     }
 
-    // Loop through student courses
+    // Loop through students courses
     for(int i = 0; i < size; i++)
     {
-        // Get percentage from current student
-        double percentage = student.courses[i].percentage;
+        // Get percentage from current students
+        double percentage = students.courses[i].percentage;
 
-        // Get Credit hours from current student
-        int credit = student.courses[i].credit;
+        // Get Credit hours from current students
+        int credit = students.courses[i].credit;
 
         // Add hours directly to total houts
         totalhours += credit;
@@ -107,6 +108,6 @@ void CalculateGPA(Student& student, int size)
 
     }
 
-    // Put student GPA into profile
-    student.GPA = totalpoints/totalhours;  
+    // Put students GPA into profile
+    students.GPA = totalpoints/totalhours;  
 }
