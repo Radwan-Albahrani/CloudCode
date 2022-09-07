f87c9eab7ff56d47747ff7061f249d2420f89bc3
Radwan albahrani <radwan158@gmail.com>
2022-09-07 14:21:12 +0300

    why not

--- 2022-2023/Term 1/Lab 1/src/Task1.java
+++ 2022-2023/Term 1/Lab 1/src/Task1.java
@@ -9,9 +9,16 @@ public class Task1
         // Prepare Variables
         int BirthYear, CurrentYear = 2022, age;
 
-        // Get Birth Year
-        System.out.print("Please Enter your Birth Year: ");
-        BirthYear = input.nextInt();
+        // Get Birth Year and make sure its not bigger than current year
+        do
+        {
+            System.out.print("Please Enter your Birth Year: ");
+            BirthYear = input.nextInt();
+            if(BirthYear > CurrentYear)
+            {
+                System.out.println("Birth Year cannot be bigger than current Year.");
+            }
+        } while(BirthYear > CurrentYear);
 
         // Calculate age
         age = CurrentYear - BirthYear;
