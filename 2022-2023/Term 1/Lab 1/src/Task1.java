16169deca52b789a669f5eb12c1b2c41dc292e01
Radwan albahrani <radwan158@gmail.com>
2022-09-07 14:27:24 +0300

    proper snake case

--- 2022-2023/Term 1/Lab 1/src/Task1.java
+++ 2022-2023/Term 1/Lab 1/src/Task1.java
@@ -8,21 +8,21 @@ public class Task1
         Scanner input = new Scanner(System.in);
 
         // Prepare Variables
-        int BirthYear, CurrentYear = Calendar.getInstance().get(Calendar.YEAR), age;
+        int birthYear, currentYear = Calendar.getInstance().get(Calendar.YEAR), age;
 
         // Get Birth Year and make sure its not bigger than current year
         do
         {
             System.out.print("Please Enter your Birth Year: ");
-            BirthYear = input.nextInt();
-            if(BirthYear > CurrentYear)
+            birthYear = input.nextInt();
+            if(birthYear > currentYear)
             {
                 System.out.println("Birth Year cannot be bigger than current Year.");
             }
-        } while(BirthYear > CurrentYear);
+        } while(birthYear > currentYear);
 
         // Calculate age
-        age = CurrentYear - BirthYear;
+        age = currentYear - birthYear;
 
         // Print age
         System.out.println("Your Age is " + age + " Years");
