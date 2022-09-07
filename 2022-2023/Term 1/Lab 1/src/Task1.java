f4c3de0f0f7a5be4cceccdeeec195add6cb3968c
Radwan albahrani <radwan158@gmail.com>
2022-09-07 14:12:32 +0300

    some modifications

--- 2022-2023/Term 1/Lab 1/src/Task1.java
+++ 2022-2023/Term 1/Lab 1/src/Task1.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
-
 public class Task1
 {
     public static void main(String[] args) throws Exception
     {
+        // Create Input object
         Scanner input = new Scanner(System.in);
+
+        // Prepare Variables
         int BirthYear, CurrentYear = 2022, age;
 
+        // Get Birth Year
         System.out.print("Please Enter your Birth Year: ");
         BirthYear = input.nextInt();
+
+        // Calculate age
         age = CurrentYear - BirthYear;
 
+        // Print age
         System.out.println("Your Age is " + age + " Years");
         input.close();
     }
