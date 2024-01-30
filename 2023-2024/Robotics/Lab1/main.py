95b8620196a882c208b46f032f627ec96ce75d84
Radwan-Albahrani <radwan158@gmail.com>
2024-01-30 13:48:01 +0300

    Lab 1 of Robotics

--- 2023-2024/Robotics/Lab1/main.py
+++ 2023-2024/Robotics/Lab1/main.py
@@ -1,27 +1,26 @@
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (
-    Motor,
-    TouchSensor,
-    ColorSensor,
-    InfraredSensor,
-    UltrasonicSensor,
-    GyroSensor,
-)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 
+# Create your objects here
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
+# Initialize the EV3 Brick.
 ev3 = EV3Brick()
 
-print("Beeping")
+# Initialize a motor at port B.
+left_wheel = Motor(Port.B)
+right_wheel = Motor(Port.C)
 
-# Make it move
-while True:
-    print(ev3.buttons.pressed())  # Wait for any button to be pressed
+# Connect Wheels
+robot = DriveBase(left_wheel, right_wheel, 56, 114)
+
+# Play a sound.
+ev3.speaker.beep()
+
+# Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
+robot.straight(500)
+
+# Play another beep sound.
+# ev3.speaker.beep(frequency=1000, duration=5000)
