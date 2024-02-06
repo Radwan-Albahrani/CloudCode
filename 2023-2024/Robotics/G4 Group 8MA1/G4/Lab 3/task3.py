c2ab77c989bea12acd86701f53ad734a354b79c9
Radwan-Albahrani <radwan158@gmail.com>
2024-02-06 13:27:31 +0300

    Robotics

---
+++ 2023-2024/Robotics/G4 Group 8MA1/G4/Lab 3/task3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.ev3devices import *
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import Motor, UltrasonicSensor
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+from pybricks.parameters import (
+    Port,
+    Button,
+    Color,
+    ImageFile,
+    SoundFile,
+    Stop,
+    Direction,
+    Align,
+)
